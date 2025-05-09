83bbfc11a7fb814647cd12002e92dc73e03c0263
Devdat Kumar <Devdat_Kumar@Outlook.com>
2025-05-09 11:32:01 +0500

    fixed landing page button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function Home() {
   return (
     <div>
-      <Button>Click!</Button>
+      <Button asChild>
+        <Link href="/signin">Click!</Link>
+      </Button>
     </div>
   );
 }
